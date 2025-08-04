a374b938a6d4d3af2e2dfb7f0087cba8a491bcfc
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2025-08-04 18:57:10 +0200

    fix: use inspect to turn action into something loggable

--- lib/ash_ai.ex
+++ lib/ash_ai.ex
@@ -1171,7 +1171,7 @@ defmodule AshAi do
     e ->
       Logger.error(
         """
-        Error raised while checking permissions for #{inspect(resource)}.#{action}
+        Error raised while checking permissions for #{inspect(resource)}.#{inspect(action)}
 
         When checking permissions, we check the action using an empty input.
         Your action should be prepared for this.
