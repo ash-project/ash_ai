2f7cc2330c16c419a5d9f1e87a17185710e5e5e6
Conor Sinclair <conor@sinclair.software>
2026-04-25 16:29:06 +0200

    chore: add phoenix_html as optional dep for OAuth consent view

--- mix.exs
+++ mix.exs
@@ -160,6 +160,7 @@ defmodule AshAi.MixProject do
       {:ash_phoenix, "~> 2.0", optional: true},
       {:igniter, "~> 0.5", optional: true},
       {:plug, "~> 1.17", optional: true},
+      {:phoenix_html, "~> 4.0", optional: true},
       {:ash_authentication, "~> 4.8", optional: true},
       # dev/test deps
       {:phx_new, "~> 1.7", optional: true},
